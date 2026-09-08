421b391503d283e3aec10c275d9ea7141e865cca
Ayush Singh <studentuse33@gmail.com>
2026-09-09 03:09:47 +0530

    Time: 267 ms (28.46%), Space: 131.2 MB (50.62%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,15 +1,21 @@
 class Solution(object):
-    def combine(self, n, k):   
-        result = []
-        def backtrack(start, path):
-            if len(path) == k:
-                result.append(path[:])   
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        ans=[]
+        path=[]
+        def back(i):
+            if len(path)==k:
+                ans.append(path[:])
                 return
-            for i in range(start, n + 1):
-                path.append(i)             
-                backtrack(i + 1, path)     
-                path.pop()                  
-        backtrack(1, [])
-        return result
-
-        
+            if i > n:
+                return
+            path.append(i)
+            back(i+1)
+            path.pop()
+            back(i+1)
+        back(1)
+        return ans
