912123d33965714515c05a95e1b7d358135ff6b1
Ayush Singh <studentuse33@gmail.com>
2026-09-09 02:58:20 +0530

    Time: 4 ms (17.76%), Space: 12.4 MB (21.9%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        ans=[]
+        path=[]
+        def back(i,k,target):
+            if len(path)==k:
+                if target==0:
+                    ans.append(path[:])
+                    return
+            if i==10:
+                return
+            if i <= target:
+                path.append(i)
+                back(i+1,k,target-i)
+                path.pop()
+            back(i+1,k,target)
+        back(1,k,n)
+        return ans
+
+        
