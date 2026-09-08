8795bcc151ccb38eb0b097d2904b7177a2247519
Ayush Singh <studentuse33@gmail.com>
2026-09-08 23:18:05 +0530

    Time: 0 ms (100%), Space: 12.5 MB (58.12%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans=[]
+        path=[]
+        def backtrack(i):
+            if i==len(nums):
+                ans.append(path[:])
+                return
+            path.append(nums[i])
+            backtrack(i+1)
+            path.pop()
+            backtrack(i+1)
+        backtrack(0)
+        return ans
+        
